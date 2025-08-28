828fc3d630080f8ba155bec8f73ba5bb59bcd66c
AnushaV01 <anusha.vendra0126@gmail.com>
2025-08-28 17:46:38 -0400

    compute: add web security group (ingress tcp/80 from internet, allow all egress)

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -7,3 +7,23 @@ data "aws_ami" "amazon_linux" {
     values = ["al2023-ami-*-x86_64"]
   }
 }
+
+resource "aws_security_group" "web" {
+name   = "web-sg"
+vpc_id = var.vpc_id
+tags   = var.tags
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http" {
+security_group_id = aws_security_group.web.id
+cidr_ipv4         = "0.0.0.0/0"
+from_port         = 80
+to_port           = 80
+ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_egress_rule" "all" {
+  security_group_id = aws_security_group.web.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
