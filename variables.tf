0f1a06c8a00f734d1d01304fbe8f7d5480bd7c84
AnushaV01 <anusha.vendra0126@gmail.com>
2025-08-28 17:26:03 -0400

    root: add compute inputs (instance_type)

--- variables.tf
+++ variables.tf
@@ -27,3 +27,8 @@ variable "private_subnet_cidrs" {
   type    = list(string)
   default = ["10.0.100.0/24", "10.0.101.0/24", "10.0.102.0/24"]
 }
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
