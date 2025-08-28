8c9cd3e4b6f9a8d2efe22556dcffa8baa4e0362b
AnushaV01 <anusha.vendra0126@gmail.com>
2025-08-28 13:20:21 -0400

    network: add 3 public subnets across AZs

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -13,3 +13,12 @@ resource "aws_internet_gateway" "igw" {
 vpc_id = aws_vpc.this.id
 tags   = merge(var.tags, { Project = "${var.project}-igw" })
 }
+
+resource "aws_subnet" "public" {
+for_each = { for i, cidr in var.public_subnet_cidrs : i => { cidr = cidr, az = local.azs[i] } }
+vpc_id = aws_vpc.this.id
+cidr_block = each.value.cidr
+availability_zone = each.value.az
+map_public_ip_on_launch = true
+tags                    = merge(var.tags, { Project = var.project, Tier = "Public", Name = "${var.project}-public-${each.key}" })
+}
