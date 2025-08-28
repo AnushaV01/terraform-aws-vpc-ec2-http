e1468630aeb882162dd47def508cc33fbe833a1a
AnushaV01 <anusha.vendra0126@gmail.com>
2025-08-28 13:18:58 -0400

    network: attach internet gateway

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,12 +1,15 @@
 data "aws_availability_zones" "available" {}
 
-
 locals {
 azs = slice(data.aws_availability_zones.available.names, 0, 3)
 }
 
-
 resource "aws_vpc" "this" {
 cidr_block = var.vpc_cidr
 tags       = merge(var.tags, { Project = "${var.project}-vpc" })
+}
+
+resource "aws_internet_gateway" "igw" {
+vpc_id = aws_vpc.this.id
+tags   = merge(var.tags, { Project = "${var.project}-igw" })
 }
